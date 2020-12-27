35715475416035bd39e05bdcf3436332dedefee4
Michelle <michelle.npm3108@gmail.com>
2020-12-28 07:01:03 +0800

    20 new words added + js

--- index.js
+++ index.js
@@ -11,3 +11,129 @@ document.getElementById('open-modal-2').addEventListener('click', function(){
 document.getElementById('close-modal-2').addEventListener('click', function(){
     document.getElementById('overlay-2').style.display='none'
 })
+
+document.getElementById('open-modal-3').addEventListener('click', function(){
+    document.getElementById('overlay-3').style.display='block'
+})
+document.getElementById('close-modal-3').addEventListener('click', function(){
+    document.getElementById('overlay-3').style.display='none'
+})
+
+document.getElementById('open-modal-4').addEventListener('click', function(){
+    document.getElementById('overlay-4').style.display='block'
+})
+document.getElementById('close-modal-4').addEventListener('click', function(){
+    document.getElementById('overlay-4').style.display='none'
+})
+
+document.getElementById('open-modal-5').addEventListener('click', function(){
+    document.getElementById('overlay-5').style.display='block'
+})
+document.getElementById('close-modal-5').addEventListener('click', function(){
+    document.getElementById('overlay-5').style.display='none'
+})
+
+document.getElementById('open-modal-6').addEventListener('click', function(){
+    document.getElementById('overlay-6').style.display='block'
+})
+document.getElementById('close-modal-6').addEventListener('click', function(){
+    document.getElementById('overlay-6').style.display='none'
+})
+
+document.getElementById('open-modal-7').addEventListener('click', function(){
+    document.getElementById('overlay-7').style.display='block'
+})
+document.getElementById('close-modal-7').addEventListener('click', function(){
+    document.getElementById('overlay-7').style.display='none'
+})
+
+document.getElementById('open-modal-8').addEventListener('click', function(){
+    document.getElementById('overlay-8').style.display='block'
+})
+document.getElementById('close-modal-8').addEventListener('click', function(){
+    document.getElementById('overlay-8').style.display='none'
+})
+
+document.getElementById('open-modal-9').addEventListener('click', function(){
+    document.getElementById('overlay-9').style.display='block'
+})
+document.getElementById('close-modal-9').addEventListener('click', function(){
+    document.getElementById('overlay-9').style.display='none'
+})
+
+document.getElementById('open-modal-10').addEventListener('click', function(){
+    document.getElementById('overlay-10').style.display='block'
+})
+document.getElementById('close-modal-10').addEventListener('click', function(){
+    document.getElementById('overlay-10').style.display='none'
+})
+
+document.getElementById('open-modal-11').addEventListener('click', function(){
+    document.getElementById('overlay-11').style.display='block'
+})
+document.getElementById('close-modal-11').addEventListener('click', function(){
+    document.getElementById('overlay-11').style.display='none'
+})
+
+document.getElementById('open-modal-12').addEventListener('click', function(){
+    document.getElementById('overlay-12').style.display='block'
+})
+document.getElementById('close-modal-12').addEventListener('click', function(){
+    document.getElementById('overlay-12').style.display='none'
+})
+
+document.getElementById('open-modal-13').addEventListener('click', function(){
+    document.getElementById('overlay-13').style.display='block'
+})
+document.getElementById('close-modal-13').addEventListener('click', function(){
+    document.getElementById('overlay-13').style.display='none'
+})
+
+document.getElementById('open-modal-14').addEventListener('click', function(){
+    document.getElementById('overlay-14').style.display='block'
+})
+document.getElementById('close-modal-14').addEventListener('click', function(){
+    document.getElementById('overlay-14').style.display='none'
+})
+
+document.getElementById('open-modal-15').addEventListener('click', function(){
+    document.getElementById('overlay-15').style.display='block'
+})
+document.getElementById('close-modal-15').addEventListener('click', function(){
+    document.getElementById('overlay-15').style.display='none'
+})
+
+document.getElementById('open-modal-16').addEventListener('click', function(){
+    document.getElementById('overlay-16').style.display='block'
+})
+document.getElementById('close-modal-16').addEventListener('click', function(){
+    document.getElementById('overlay-16').style.display='none'
+})
+
+document.getElementById('open-modal-17').addEventListener('click', function(){
+    document.getElementById('overlay-17').style.display='block'
+})
+document.getElementById('close-modal-17').addEventListener('click', function(){
+    document.getElementById('overlay-17').style.display='none'
+})
+
+document.getElementById('open-modal-18').addEventListener('click', function(){
+    document.getElementById('overlay-18').style.display='block'
+})
+document.getElementById('close-modal-18').addEventListener('click', function(){
+    document.getElementById('overlay-18').style.display='none'
+})
+
+document.getElementById('open-modal-19').addEventListener('click', function(){
+    document.getElementById('overlay-19').style.display='block'
+})
+document.getElementById('close-modal-19').addEventListener('click', function(){
+    document.getElementById('overlay-19').style.display='none'
+})
+
+document.getElementById('open-modal-20').addEventListener('click', function(){
+    document.getElementById('overlay-20').style.display='block'
+})
+document.getElementById('close-modal-10').addEventListener('click', function(){
+    document.getElementById('overlay-10').style.display='none'
+})
