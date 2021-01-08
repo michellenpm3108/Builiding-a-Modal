91b1fa2aa998427833ae45000583590fe623a189
Michelle <michelle.npm3108@gmail.com>
2021-01-09 04:46:06 +0800

    revise css and js

--- test.js
+++ test.js
@@ -1,32 +1,31 @@
+const meaningBtn = document.getElementsByClassName('meaning-btn')
+const closeMeaningBtn = document.getElementsByClassName('close-meaning-btn')
+const meaningDiv = document.getElementsByClassName('meaning-div')
 
-const openModal = document.querySelectorAll('#open-modal')
-const closeModal = document.querySelectorAll('#close-modal')
-const meaningDiv = document.querySelectorAll('#meaning-div')
-for (let i=0 ; i<openModal.length ; i++) {
-    openModal[i].addEventListener('click', function(){
-        meaningDiv[i].style.display = 'block'
-    }) 
-}
-
-for (let i=0 ; i<closeModal.length ; i++) {
-    closeModal[i].addEventListener('click', function(){
-        meaningDiv[i].style.display = 'none'
+for (let i=0 ; i < meaningBtn.length ; i++) {
+    meaningBtn[i].addEventListener('click', function(){
+        meaningDiv[i].style.display='block'
     })
-   
 }
+for (let i=0 ; i < closeMeaningBtn.length ; i++) {
+    closeMeaningBtn[i].addEventListener('click', function(){
+        meaningDiv[i].style.display='none'
+    })
+}
+
 
 const searchInput = document.getElementById('searchInput')
+const wordFrame = document.querySelectorAll('.new-word')
 const allWords = document.querySelectorAll('.word')
-const frameWord = document.querySelectorAll('.new-word')
-
 searchInput.addEventListener('keyup', function(event){
-    let searchQuery = event.target.value.toLowerCase()
-    for (let m=0 ; m < allWords.length ; m++) {
-        let currentWord = allWords[m].innerHTML.toLowerCase()
-        if (currentWord.includes(searchQuery)) {
-            frameWord[m].style.display = "block"
+    const searchQuery = event.target.value.toLowerCase();
+    for (let i=0 ; i < allWords.length ; i++) {
+        const currentWord = allWords[i].innerHTML.toLowerCase()
+        if (currentWord.includes(searchQuery)){
+            wordFrame[i].style.display='block'
         } else {
-            frameWord[m].style.display= "none"
+            wordFrame[i].style.display='none'
         }
     }
+
 })
